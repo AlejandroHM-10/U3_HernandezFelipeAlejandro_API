7b3a325e9f6462520b35676784aceda16f450769
Alejandro <felipe_60@live.com.mx>
2021-06-02 17:26:51 -0500

    Fix

--- src/main/java/mx/edu/utez/app/controller/PersonaController.java
+++ src/main/java/mx/edu/utez/app/controller/PersonaController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +27,7 @@ public class PersonaController {
 	}
 	
 	@GetMapping("/get/{id}")
-	public Persona getAll(@PathVariable long id){
+	public Persona get(@PathVariable long id){
 		return personaService.findOne(id);
 	}
 	
@@ -38,7 +39,7 @@ public class PersonaController {
 			return persona;
 		}
 	}
-	@PostMapping("/update")
+	@PutMapping("/update")
 	public Persona actualizar(@RequestBody Persona persona) {
 		try {
 			return personaService.save(persona);
--- src/main/java/mx/edu/utez/app/entity/Persona.java
+++ src/main/java/mx/edu/utez/app/entity/Persona.java
@@ -32,5 +32,97 @@ public class Persona {
 	private int numeroInterior;
 	private String correo;
 	private Date fechaRegistro;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidoPaterno() {
+		return apellidoPaterno;
+	}
+	public void setApellidoPaterno(String apellidoPaterno) {
+		this.apellidoPaterno = apellidoPaterno;
+	}
+	public String getApellidoMaterno() {
+		return apellidoMaterno;
+	}
+	public void setApellidoMaterno(String apellidoMaterno) {
+		this.apellidoMaterno = apellidoMaterno;
+	}
+	public String getRfc() {
+		return rfc;
+	}
+	public void setRfc(String rfc) {
+		this.rfc = rfc;
+	}
+	public String getCurp() {
+		return curp;
+	}
+	public void setCurp(String curp) {
+		this.curp = curp;
+	}
+	public String getClaveElector() {
+		return claveElector;
+	}
+	public void setClaveElector(String claveElector) {
+		this.claveElector = claveElector;
+	}
+	public String getEnditdadFederativa() {
+		return enditdadFederativa;
+	}
+	public void setEnditdadFederativa(String enditdadFederativa) {
+		this.enditdadFederativa = enditdadFederativa;
+	}
+	public String getMunicipio() {
+		return municipio;
+	}
+	public void setMunicipio(String municipio) {
+		this.municipio = municipio;
+	}
+	public String getColonia() {
+		return colonia;
+	}
+	public void setColonia(String colonia) {
+		this.colonia = colonia;
+	}
+	public String getCalle() {
+		return calle;
+	}
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+	public int getNumeroExterior() {
+		return numeroExterior;
+	}
+	public void setNumeroExterior(int numeroExterior) {
+		this.numeroExterior = numeroExterior;
+	}
+	public int getNumeroInterior() {
+		return numeroInterior;
+	}
+	public void setNumeroInterior(int numeroInterior) {
+		this.numeroInterior = numeroInterior;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public Date getFechaRegistro() {
+		return fechaRegistro;
+	}
+	public void setFechaRegistro(Date fechaRegistro) {
+		this.fechaRegistro = fechaRegistro;
+	}
+	
+	
 
 }
